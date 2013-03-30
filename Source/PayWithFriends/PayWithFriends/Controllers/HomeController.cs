9109b1fbf13f9dd57f1b1605137a6a41c630a6c9
Spawek <spawnerer@gmail.com>
2013-03-31 00:22:10 +0100

    moved Scripts to proper directory MVC and SQL learning

--- Source/PayWithFriends/PayWithFriends/Controllers/HomeController.cs
+++ Source/PayWithFriends/PayWithFriends/Controllers/HomeController.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PayWithFriends.Models;
 
 namespace PayWithFriends.Controllers
 {
     public class HomeController : Controller
     {
+        private DbClassesDataContext db = new DbClassesDataContext();
+
         public ActionResult Index()
         {
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
 
-            return View();
+            var data = from r in db.Friends
+                       orderby r.senderID ascending
+                       select r;
+
+            return View(data);
         }
 
         public ActionResult About()
@@ -27,6 +34,15 @@ namespace PayWithFriends.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if(db != null)
+            {
+                db.Dispose();
+            }
+ 	        base.Dispose(disposing);
         }
     }
 }
